e33684d14fd24d4e2a3222b27b5e96bcd9b25c6f
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-08-04 23:29:53 +0200

    learning rate decays added

--- nnlib/network.py
+++ nnlib/network.py
@@ -36,6 +36,42 @@ class Losses:
         mse.__func__: mse_grad.__func__
     }
 
+class LrDecays:
+    @staticmethod
+    def exponential_decay(current_lr, factor):
+        return factor * current_lr
+
+    @staticmethod
+    def step_decay(current_lr, lr_drop, epoch, epoch_interval):
+        if epoch % epoch_interval == 0:
+            return current_lr - lr_drop
+        else:
+            return current_lr
+
+    @staticmethod
+    def linear_decay(current_lr, lr_drop, lr_min):
+        return max(lr_min, current_lr - lr_drop)
+
+    @staticmethod
+    def cosine_decay(current_epoch, total_epochs, start_lr, min_lr):
+        current_epoch = min(current_epoch, total_epochs)
+        progress = current_epoch / total_epochs
+        cosine_out = 0.5 * (1.0 + np.cos(np.pi * progress))
+        lr = min_lr + (start_lr - min_lr) * cosine_out
+        return lr
+
+    @staticmethod
+    def SGDR(current_epoch, start_cycle_epochs, cycle_multiplier, start_lr, min_lr):
+        cycle_epochs = start_cycle_epochs
+        epoch_in_cycle = current_epoch
+        while epoch_in_cycle >= cycle_epochs:
+            epoch_in_cycle -= cycle_epochs
+            cycle_epochs *= cycle_multiplier
+        progress = epoch_in_cycle / cycle_epochs
+        cosine_out = 0.5 * (1.0 + np.cos(np.pi * progress))
+        lr = min_lr + (start_lr - min_lr) * cosine_out
+        return lr
+
 class Layer:
     def __init__(self, n_in, n_out, activation):
         self.weights = np.random.randn(n_in, n_out) * 0.1
@@ -59,9 +95,8 @@ class Layer:
         self.biases -= learning_rate * self.bias_gradient
 
 class Network:
-    def __init__(self, loss_function=Losses().mse, input_size=0, hidden_size=0, hidden_amount=0, output_size=0, window_width=0, window_height=0):
+    def __init__(self, loss_function, input_size=0, hidden_size=0, hidden_amount=0, output_size=0, window_width=0, window_height=0):
         self.layers = []
-        self.loss_function = loss_function
         self.num_layers = 0
         self.input_size = 0
         self.output_size = 0
@@ -76,11 +111,14 @@ class Network:
         self.screen = None
         self.window_width = window_width
         self.window_height = window_height
+        self.epoch = 0
+        self.loss = 1 # should be updated in the training loop
 
     def add(self, layer):
         self.layers.append(layer)
 
     def forward(self, inputs):
+        self.epochs += 1
         output = inputs
         for layer in self.layers:
             output = layer.forward(output)
@@ -160,4 +198,6 @@ class Network:
                     color = (0, 134, 212)
                 end_y = (self.window_height / 2) - (node_gap * (self.output_size - 1 / 2)) + (p * node_gap)
                 pygame.draw.line(self.screen, color, (start_x, start_y), (end_x, end_y), max(1, int(abs(weight) * 7)))
+        text = self.font.render(f"epoch: {self.epoch} | loss: {self.loss}", True, (255, 255, 255))
+        self.screen.blit(text, (self.window_width / 2 - text.get_width() / 2, (vert_side_offset - text.get_height()) / 2))
         pygame.display.update()
