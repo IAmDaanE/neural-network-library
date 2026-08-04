b13839e279e722eeac07fbb68794b1a80cdfc51a
Daan Eeckloo <daan.eeckloo@gmail.com>
2026-08-04 15:06:26 +0200

    visualization bug fix attempt

--- nnlib/network.py
+++ nnlib/network.py
@@ -36,7 +36,6 @@ class Losses:
         mse.__func__: mse_grad.__func__
     }
 
-
 class Layer:
     def __init__(self, n_in, n_out, activation):
         self.weights = np.random.randn(n_in, n_out) * 0.1
@@ -154,7 +153,7 @@ class Network:
             end_x = hor_side_offset + hor_gap * (self.hidden_amount + 1)
             start_y = self.window_height / 2 - node_gap * (self.hidden_size / 2) + q * node_gap
             for p in range(self.output_size):
-                weight = self.layers[self.hidden_amount].weights[q, p]
+                weight = self.layers[self.hidden_amount + 1].weights[q, p]
                 if weight > 0:
                     color = (255, 255, 255)
                 else:
