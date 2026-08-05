softmax losses fixed

nnlib/network.py
@@ -54,20 +54,22 @@ class Losses:
 
     @staticmethod
     def softmax_cross_entropy(prediction_logits, true_value):
-        batch_size = prediction_logits.shape[0]
+        num_samples = prediction_logits.shape[0]
         shift_logits = prediction_logits - np.max(prediction_logits, axis=-1, keepdims=True)
         exps = np.exp(shift_logits)
         softmax_output = exps / np.sum(exps, axis=-1, keepdims=True)
         softmax_output = np.clip(softmax_output, 1e-15, 1.0 - 1.0e-15)
-        return -np.sum(true_value * np.log(softmax_output)) / batch_size
+        loss_sum = -np.sum(true_value * np.log(softmax_output))
+        return loss_sum / num_samples
+
 
     @staticmethod
     def softmax_cross_entropy_grad(prediction_logits, true_value):
-        batch_size = prediction_logits.shape[0]
+        num_samples = prediction_logits.shape[0]
         shift_logits = prediction_logits - np.max(prediction_logits, axis=-1, keepdims=True)
         exps = np.exp(shift_logits)
         softmax_output = exps / np.sum(exps, axis=-1, keepdims=True)
-        return (softmax_output - true_value) / batch_size
+        return (softmax_output - true_value) / num_samples
 
     gradient_map = {
         mse.__func__: mse_grad.__func__,
@@ -127,6 +129,16 @@ class WeightInitializers:
     @staticmethod
     def random_small(n_in, n_out): # algemeen
         return np.random.randn(n_in, n_out) * 0.01
+
+class PreTrainedLayer:
+    def __init__(self, weights_location, biases_location, activation):
+        self.weights = np.load(weights_location)
+        self.biases = np.load(biases_location)
+        self.activation = activation
+
+    def forward(self, inputs):
+        self.pre_activation = inputs @ self.weights + self.biases
+        return self.activation(self.pre_activation)
 
 class Layer:
     def __init__(self, n_in, n_out, activation, initializer):
@@ -254,13 +266,3 @@ class Network:
                 pygame.quit()
                 self.screen = None
                 return
-
-class PreTrainedLayer:
-    def __init__(self, weights_location, biases_location, activation):
-        self.weights = np.load(weights_location)
-        self.biases = np.load(biases_location)
-        self.activation = activation
-
-    def forward(self, inputs):
-        self.pre_activation = inputs @ self.weights + self.biases
-        return self.activation(self.pre_activation)